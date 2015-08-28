a5e3cd6d645ba72efec1532069d29fd2c06dbb51
Aaron Probus <pair+aaron@fundingcircle.com>
2015-08-28 14:03:45 -0700

    Swaps period/frequency namespaces
    The period namespace was limiting on frequency, and the frequency
    namespace was limiting on period. The namespaces make logical sense now.

--- spec/ring_venturi/period_spec.clj
+++ spec/ring_venturi/period_spec.clj
@@ -1,157 +1,33 @@
 (ns ring-venturi.period-spec
-  (:require [ring-venturi.cache :as cache]
-            [ring-venturi.period :refer :all]
+  (:require [ring-venturi.period :as period]
             [speclj.core :refer :all]
-            [clj-time.core :as t]
             speclj.run.standard))
 
-(defn- inc-in [m key]
-  (let [current-value (get m key 0)]
-    (assoc m key (inc current-value))))
+(describe "in-memory-limiter"
+  (with handler (constantly {:status 200}))
+  (with limiter (period/in-memory-limiter 1000))
+  (with app (period/limit @handler @limiter :id ))
+  (with request {:id 1})
 
-(defrecord AtomCache [atom-cache]
-  cache/PeriodCache
+  (describe "the first request"
+    (it "gets through"
+        (should= 200 (:status (@app @request)))))
 
-  (get-request-counts [this keys]
-    (map #(get @atom-cache %1 0) keys))
+  (describe "a second request within the backoff"
+    (before (@app @request))
+    (it "is blocked"
+        (should= 429 (:status (@app @request)))))
 
-  (inc-request-count [this key expire]
-    (swap! atom-cache inc-in key)))
+  (describe "a second request after the backoff"
+    (before (do
+              (@app @request)
+              (Thread/sleep 1000)))
+    (it "gets through"
+        (should= 200 (:status (@app @request)))))
 
-(describe "rate-limit-spec"
-  (with cache (AtomCache. (atom {})))
-
-  (around [it]
-    (with-redefs [t/now (constantly (t/date-time 2015 6 14 9 31 27 456))]
-      (it)))
-
-  (describe "limit-requests-per-hour"
-    (describe "when under limit"
-      (with limit-fn (limit-requests-per-hour (constantly {:status 200})
-                                              @cache
-                                              {:limit 2
-                                               :identifier-fn :id}))
-
-      (before
-        ; This request should be ignored
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T08:31" 3600)
-        ; This request should be counted
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T09:30" 3600))
-
-      (it "forwards request when under limit"
-        (should= 200
-                 (:status (@limit-fn {:id 1}))))
-
-      (it "increments the request count"
-        (should= 1
-                 (do
-                   (@limit-fn {:id 1})
-                   (get (deref (:atom-cache @cache)) "rate-limit-1-2015-06-14T09:31")))))
-
-    (describe "when over limit"
-      (with limit-fn (limit-requests-per-hour (constantly {:status 200})
-                                              @cache
-                                              {:limit 2
-                                               :identifier-fn :id}))
-
-      (before
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T08:32" 3600)
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T09:29" 3600))
-
-      (it "has too many requests"
-        (should= 429
-                 (:status (@limit-fn {:id 1}))))
-
-      (it "does not increment the request count"
-        (should-not (do
-                      (@limit-fn {:id 1})
-                      (get (deref (:atom-cache @cache))
-                           "rate-limit-1-2015-06-14T09:31"))))))
-
-  (describe "limit-requests-per-minute"
-    (describe "when under limit"
-      (with limit-fn (limit-requests-per-minute (constantly {:status 200})
-                                                @cache
-                                                {:limit 2
-                                                 :identifier-fn :id}))
-
-      (before
-        ; This request should be ignored
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T09:30:27" 3600)
-        ; This request should be counted
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T09:31:26" 3600))
-
-      (it "forwards request when under limit"
-        (should= 200
-                 (:status (@limit-fn {:id 1}))))
-
-      (it "increments the request count"
-        (should= 1
-                 (do
-                   (@limit-fn {:id 1})
-                   (get (deref (:atom-cache @cache)) "rate-limit-1-2015-06-14T09:31:27")))))
-
-    (describe "when over limit"
-      (with limit-fn (limit-requests-per-minute (constantly {:status 200})
-                                                @cache
-                                                {:limit 2
-                                                 :identifier-fn :id}))
-
-      (before
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T09:30:28" 3600)
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T09:31:26" 3600))
-
-      (it "has too many requests"
-        (should= 429
-                 (:status (@limit-fn {:id 1}))))
-
-      (it "does not increment the request count"
-        (should-not (do
-                      (@limit-fn {:id 1})
-                      (get (deref (:atom-cache @cache))
-                           "rate-limit-1-2015-06-14T09:31:27"))))))
-
-  (describe "limit-requests-per-second"
-    (describe "when under limit"
-      (with limit-fn (limit-requests-per-second (constantly {:status 200})
-                                                @cache
-                                                {:limit 2
-                                                 :identifier-fn :id}))
-
-      (before
-        ; This request should be ignored
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T09:31:26:4" 3600)
-        ; This request should be counted
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T09:31:27:3" 3600))
-
-      (it "forwards request when under limit"
-        (should= 200
-                 (:status (@limit-fn {:id 1}))))
-
-      (it "increments the request count"
-        (should= 1
-                 (do
-                   (@limit-fn {:id 1})
-                   (get (deref (:atom-cache @cache)) "rate-limit-1-2015-06-14T09:31:27:4")))))
-
-    (describe "when over limit"
-      (with limit-fn (limit-requests-per-second (constantly {:status 200})
-                                                @cache
-                                                {:limit 2
-                                                 :identifier-fn :id}))
-
-      (before
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T09:31:26:5" 3600)
-        (.inc-request-count @cache "rate-limit-1-2015-06-14T09:31:27:1" 3600))
-
-      (it "has too many requests"
-        (should= 429
-                 (:status (@limit-fn {:id 1}))))
-
-      (it "does not increment the request count"
-        (should-not (do
-                      (@limit-fn {:id 1})
-                      (get (deref (:atom-cache @cache))
-                           "rate-limit-1-2015-06-14T09:31:27:4")))))))
+  (describe "when request has no user"
+    (before (@app {}))
+    (it "gets through"
+        (should= 200 (:status (@app {}))))))
 
 (run-specs)
--- src/ring_venturi/period.clj
+++ src/ring_venturi/period.clj
@@ -1,99 +1,30 @@
 (ns ring-venturi.period
-  (:require [clj-time.core :as t]
-            [clj-time.format :as f]
-            [clj-time.periodic :as p]
-            [clojure.data.json :as json]))
+  (:require [clojure.core.cache :as cache]
+            [clj-time.core :as t]
+            [clj-time.coerce :as c]))
 
-(def ^:private exceeded-limit-response
-  {:status 429
-   :body (json/write-str {:message "Exceeded limit"})})
-
-(def ^:private format-time-to-mins
-  (partial f/unparse
-           (f/formatter "yyyy-MM-dd'T'hh:mm")))
-
-(def ^:private format-time-to-sec
-  (partial f/unparse
-           (f/formatter "yyyy-MM-dd'T'hh:mm:ss")))
-
-(def ^:private format-time-to-millis
-  (partial f/unparse
-           (f/formatter "yyyy-MM-dd'T'hh:mm:ss:S")))
+(def backoff-response {:status 429})
 
 (defprotocol PeriodRateLimiter
-  (time-seq [this now]
-            "Sequence of time strings, from newest to oldest,
-            that are relevant to the given time.
-            (Limiting requests by hour should be a sequence of
-            60 times differing by one minute)")
-  (inc-cache [this cache bucket-key]
-             "Increments or sets the number of requests for a given bucket key.
-             (Limiting requests by hour should expire the key after an hour)"))
+  "Determines if the request is too soon. Returns true if no other requests have been made
+  in the period, false otherwise."
+  (try-update-key [this k]))
 
-(defrecord HourRateLimiter [limit]
+(deftype InMemoryLimiter [ttl-cache]
   PeriodRateLimiter
-  (time-seq [this now]
-    (map format-time-to-mins (take 60
-                                   (p/periodic-seq now (t/minutes -1)))))
-  (inc-cache [this cache bucket-key]
-    (.inc-request-count cache bucket-key 3600)))
+  (try-update-key [this k]
+    (if (cache/has? @ttl-cache k)
+      false
+      (swap! ttl-cache cache/miss k true))))
 
-(defrecord MinuteRateLimiter [limit]
-  PeriodRateLimiter
-  (time-seq [this now]
-    (map format-time-to-sec (take 60
-                                  (p/periodic-seq now (t/seconds -1)))))
-  (inc-cache [this cache bucket-key]
-    (.inc-request-count cache bucket-key 60)))
+(defn in-memory-limiter [ttl-millis]
+  (InMemoryLimiter.
+   (atom (cache/ttl-cache-factory {} :ttl ttl-millis))))
 
-(defrecord SecondRateLimiter [limit]
-  PeriodRateLimiter
-  (time-seq [this now]
-    (map format-time-to-millis (take 10
-                                     (p/periodic-seq now (t/millis -100)))))
-  (inc-cache [this cache bucket-key]
-    (.inc-request-count cache bucket-key 1)))
-
-(defn- bucket-seq [request-id rate-limiter]
-  (let [time-seq (.time-seq rate-limiter (t/now))]
-    (map #(str "rate-limit-" request-id "-" %1) time-seq)))
-
-(defn- count-requests [cache bucket-keys]
-  "Counts the number of requests this user has made"
-  (let [request-counts (.get-request-counts cache bucket-keys)]
-    (reduce + request-counts)))
-
-(defn- exceeds-limit? [cache bucket-keys rate-limiter]
-  "Determines if another request would exceed the request limit"
-  (let [num-requests (count-requests cache bucket-keys)]
-    (>= num-requests (:limit rate-limiter))))
-
-(defn- limit-rate* [handler cache rate-limiter request request-id]
-  (let [bucket-keys (bucket-seq request-id rate-limiter)]
-    (if-not (exceeds-limit? cache bucket-keys rate-limiter)
-      (do
-        (.inc-cache rate-limiter cache (first bucket-keys))
-        (handler request))
-      exceeded-limit-response)))
-
-(defn- new-hour-limiter [limit]
-  (HourRateLimiter. limit))
-(defn- new-minute-limiter [limit]
-  (MinuteRateLimiter. limit))
-(defn- new-second-limiter [limit]
-  (SecondRateLimiter. limit))
-
-(defn- limit-requests [limiter-builder handler cache opts]
-  (let [{:keys [limit identifier-fn]} opts
-        rate-limiter (limiter-builder limit)]
-    (fn [request]
-      (let [request-id (identifier-fn request)]
-        (limit-rate* handler
-                     cache
-                     rate-limiter
-                     request
-                     request-id)))))
-
-(def limit-requests-per-hour   (partial limit-requests new-hour-limiter))
-(def limit-requests-per-minute (partial limit-requests new-minute-limiter))
-(def limit-requests-per-second (partial limit-requests new-second-limiter))
+(defn limit [handler limiter id-fn]
+  (fn [request]
+    (if-let [id (id-fn request)]
+      (if (try-update-key limiter id)
+        (handler request)
+        backoff-response)
+      (handler request))))
