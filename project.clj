f06f3774a9cfc6069d4a10914279ee76a34495cb
Francisco Viramontes <kidpollo@gmail.com>
2018-01-12 13:19:49 -0800

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "BSD 3-clause"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
   :plugins [[speclj "3.2.0"]]
-  :profiles {:dev {:dependencies [[speclj "3.2.0"]]}}
+  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
   :test-paths ["spec"]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-time "0.9.0"]
-                 [org.clojure/core.cache "0.6.4"]
-                 [clojurewerkz/spyglass "1.0.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clj-time "0.14.2"]
+                 [org.clojure/core.cache "0.6.5"]
+                 [clojurewerkz/spyglass "1.2.0"]
                  [org.clojure/data.json "0.2.6"]])
